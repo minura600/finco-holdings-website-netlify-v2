4511400cde4075289766a8fd0e2e6bb37e27ec6e
Minura Ratnasekera <minura600@gmail.com>
2022-09-07 15:44:12 +0530

    Changing collection name and introducing sorting

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 
+  // Returns work items, sorted by display order
+  eleventyConfig.addCollection('brands', collection => {
+    return collection
+      .getFilteredByGlob('./src/portfolio/*.md')
+      .sort((a, b) => (Number(a.data.displayOrder) > Number(b.data.displayOrder) ? 1 : -1));
+    });
+
   return {
     passthroughFileCopy: true,
     markdownTemplateEngine: "njk",
