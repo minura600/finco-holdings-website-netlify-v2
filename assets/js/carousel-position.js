b1afb538c794a2b053107e5bb6b6738d0d9317f8
Minura Ratnasekera <minura600@gmail.com>
2022-09-14 00:09:38 +0530

    carousel first draft

---
+++ assets/js/carousel-position.js
@@ -0,0 +1,27 @@
+function offsetCalculate(){
+    var parentTop = $('#supposed-parent').offset();
+    var parentLeft = $('#supposed-parent').offset();
+    var height = 100
+    $('#supposed-child').css({
+        'top':parentTop.top - height,
+        'left': parentLeft.left
+    });
+    $('#supposed-child1').css({
+        'top':parentTop.top - height,
+        'left': parentLeft.left
+    });
+    $('#supposed-child2').css({
+        'top':parentTop.top - height,
+        'left': parentLeft.left
+    });
+    $('#supposed-child3').css({
+        'top':parentTop.top - height,
+        'left': parentLeft.left
+    });
+}
+$(document).ready(function () {
+    offsetCalculate();
+});
+$(window).resize(function(){
+    offsetCalculate();
+});
